5d92dd93767c0a58f820de8a93555bcc48ccb911
Abonashy <ahmedabonashy1000@gmail.com>
2022-07-29 18:36:38 +0200

    initial project. Added  Home Screen

---
+++ lib/Background.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String RouteName = 'Home';
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        //body: Container(decoration: BoxDecoration(image: DecorationImage(image: AssetImage('images/default_bg.png'))),),
+        );
+  }
+}
